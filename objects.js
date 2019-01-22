6686fabd118880353b50fc4d263d184ea2c96a2f
Ryan Jasinski <jasinski.17@gmail.com>
2019-01-22 15:56:56 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,11 +3,11 @@ var playlist = {
 };
 
 function updatePlaylist (obj, artist, song) {
-  obj.artist = song;
+  obj[artist] = song;
   return obj;
 }
 
 function removeFromPlaylist (obj, artist) {
-  delete obj.artist;
+  delete obj[artist];
   return obj;
 }
